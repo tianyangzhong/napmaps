4c6fac4026e0234fc1b2e2d572061bf1f612a179
System Administrator <root@tianyangs-MacBook-Air.local>
2015-10-25 20:04:20 -0400

    removed folder

--- Desktop/work/car/car_admin/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$mysqli = new mysqli();
-$mysqli->connect("localhost", "root", "123456", "car");
-if ($mysqli -> connect_error){
-    echo "Fail to connect to MySQL (" . $mysqli -> connect_errno . ")" . $mysqli -> connect_error;
-}
-
-?>
